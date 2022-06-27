e89b6a8151661b8cb462a6c31dabf935820b73df
codelzn <lzn724958732@gmail.com>
2022-06-27 22:25:45 +0900

    update:home-css-header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 "use strict";
+class Main {
+  // burger menu
+  initBurgerMenu (controls, item) {
+    const burger = document.querySelector(controls);
+    const burgerContent = document.querySelector(item);
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      burgerContent.classList.toggle('active');
+    })
+  }
+}
+const home = new Main();
+home.initBurgerMenu('.burger', '.burger-content');
+
+export default Main;
