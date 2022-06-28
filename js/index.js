f064ce2b5927ed76835f0b494f6e88e61a0b4eb9
codelzn <lzn724958732@gmail.com>
2022-06-28 14:01:34 +0900

    update:concept-css

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   burgerContent.classList.toggle('active')
 })
+const navlinks = document.querySelectorAll('.burger-content li')
+navlinks.forEach((v, i) => {
+  v.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    burgerContent.classList.toggle('active')
+  })
+})
