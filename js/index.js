77519554619d2f120ac6398012bcaf814d5e1c2e
codelzn <lzn724958732@gmail.com>
2022-06-29 18:35:22 +0900

    update:css

--- js/index.js
+++ js/index.js
@@ -17,3 +17,21 @@ burgerContent.addEventListener('transitionend', () => {
   const ul = burgerContent.children[0]
   burgerContent.classList.value.indexOf('active') > -1 ? ul.classList.add('active') : ul.classList.remove('active')
 })
+gsap.registerPlugin(ScrollTrigger)
+// scroll animation
+const els = document.querySelectorAll('.color-section')
+els.forEach(el => {
+  let color = el.dataset.color
+  gsap.set('body', {
+    backgroundColor: '#fff'
+  })
+  gsap.to('body', {
+    backgroundColor: color,
+    scrollTrigger: {
+      trigger: el,
+      start: 'top center',
+      // end: 'bottom center',
+      toggleActions: 'play pause play reverse',
+    }
+  })
+})
