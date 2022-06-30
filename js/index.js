5b955c9f949aa99e6b31658b0ec61310dae02f3c
codelzn <lzn724958732@gmail.com>
2022-06-30 11:59:02 +0900

    update:home-css

--- js/index.js
+++ js/index.js
@@ -30,8 +30,13 @@ els.forEach(el => {
     scrollTrigger: {
       trigger: el,
       start: 'top center',
-      // end: 'bottom center',
-      toggleActions: 'play pause play reverse',
-    }
+      end: 'bottom bottom',
+      toggleActions: 'play none restart none',
+      // markers: true,
+      // onEnter: ({ progress, direction, isActive }) => console.log('onEnter', progress, direction, isActive),
+      // onEnterBack: ({ progress, direction, isActive }) => console.log('onEnterBack', progress, direction, isActive),
+      // onLeave: ({ progress, direction, isActive }) => console.log('onLeave', progress, direction, isActive),
+      // onLeaveBack: ({progress, direction, isActive}) => console.log('onLeaveBack', progress, direction, isActive)
+    },
   })
 })
