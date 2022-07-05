5862314bb3daeff864dcf8e487c7ae8d79d38095
codelzn <lzn724958732@gmail.com>
2022-07-05 16:12:40 +0900

    complete:access-css

--- competition/js/index.js
+++ competition/js/index.js
@@ -1,1 +1,19 @@
-console.log('inited');
+'use strict'
+const dialogs = document.querySelectorAll('.dialog')
+const mainSpLi = document.querySelectorAll('.mainSp ul li')
+const attrSpLi = document.querySelectorAll('.attractions ul li')
+const eventLi = [...mainSpLi, ...attrSpLi]
+eventLi.forEach((v, i) => {
+  v.addEventListener('click', e => {
+    e.stopPropagation()
+    dialogs.forEach(e => e.style.display = 'none')
+    const dialog = v.children[v.children.length - 1]
+    dialog.style.display = 'block'
+  })
+})
+dialogs.forEach((v, i) => {
+  v.addEventListener('click', e => {
+    e.stopPropagation()
+    e.target === v && (v.style.display = 'none')
+  })
+})
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger)
 // scroll animation
 const els = document.querySelectorAll('.color-section')
 els.forEach(el => {
-  let color = el.dataset.color
+  const color = el.dataset.color
   gsap.set('body', {
     backgroundColor: '#fff'
   })
@@ -14,11 +14,6 @@ els.forEach(el => {
       start: 'top center',
       end: 'bottom bottom',
       toggleActions: 'play none restart none',
-      // markers: true,
-      // onEnter: ({ progress, direction, isActive }) => console.log('onEnter', progress, direction, isActive),
-      // onEnterBack: ({ progress, direction, isActive }) => console.log('onEnterBack', progress, direction, isActive),
-      // onLeave: ({ progress, direction, isActive }) => console.log('onLeave', progress, direction, isActive),
-      // onLeaveBack: ({progress, direction, isActive}) => console.log('onLeaveBack', progress, direction, isActive)
     },
   })
 })
